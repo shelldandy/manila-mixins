0a18e4c11f83df7083c2ae681cf7c92927883328
Miguel Palau <mpalauzarza@gmail.com>
2017-01-12 11:23:25 -0600

    bump version to 2.2.0

--- sassdocs.config.js
+++ sassdocs.config.js
@@ -1,19 +1,19 @@
 module.exports = {
-  "dest" : "./dist",
-  "groups" : {
-    "undefined" : "General",
-    "layout" : "Layout",
-    "styling" : "Styling",
-    "bem" : "BEM"
+  'dest': './dist',
+  'groups': {
+    'undefined': 'General',
+    'layout': 'Layout',
+    'styling': 'Styling',
+    'bem': 'BEM'
   },
-  "package" : {
-    "title" : "Manila Mixins",
-    "name" : "manila-mixins",
-    "version" : "2.0.0",
-    "license" : "MIT",
-    "homepage" : "https://github.com/mike3run/manila-mixins",
-    "description" : "A bunch of really cool Sass Mixins"
+  'package': {
+    'title': 'Manila Mixins',
+    'name': 'manila-mixins',
+    'version': '2.2.0',
+    'license': 'MIT',
+    'homepage': 'https://github.com/mike3run/manila-mixins',
+    'description': 'A bunch of really cool Sass Mixins'
   },
-  "theme" : "flippant",
-  "verbose" : "true"
-};
+  'theme': 'flippant',
+  'verbose': 'true'
+}
