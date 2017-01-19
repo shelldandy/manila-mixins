9ba3af51cd3f39c3ff98816aac6f1ba02b79aad9
Miguel Palau <mpalauzarza@gmail.com>
2017-01-19 16:28:29 -0600

    bump version

--- sassdocs.config.js
+++ sassdocs.config.js
@@ -9,7 +9,7 @@ module.exports = {
   'package': {
     'title': 'Manila Mixins',
     'name': 'manila-mixins',
-    'version': '2.2.0',
+    'version': '2.4.0',
     'license': 'MIT',
     'homepage': 'https://github.com/mike3run/manila-mixins',
     'description': 'A bunch of really cool Sass Mixins'
