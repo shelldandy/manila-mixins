de0e0e32135f1bf033076e526d3deabec41f2708
Miguel Palau <mpalauzarza@gmail.com>
2017-07-16 18:46:43 -0500

    new shiny version released

--- docs.js
+++ docs.js
@@ -2,7 +2,7 @@ var sassdoc = require('sassdoc')
 
 var config = require('./sassdocs.config')
 
-sassdoc('./mixins/*.{sass,scss}', config)
+sassdoc('./mixins/**/_*.{sass,scss}', config)
   .then(function () {
     console.log('Your documentation has been generated!')
   }, function (err) {
